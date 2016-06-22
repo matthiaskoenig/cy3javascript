d34a0e022f5089ac08dafd5f0b1b913abeca2f60
Matthias Koenig <konigmatt@googlemail.com>
2016-06-22 17:55:01 +0200

    Minimal netscape.javascript example

--- src/main/java/org/cyadd2nodes/CyActivator.java
+++ src/main/java/org/cyadd2nodes/CyActivator.java
@@ -1,6 +1,12 @@
 package org.cyadd2nodes;
 
 import org.osgi.framework.BundleContext;
+
+import netscape.javascript.JSObject;
+import javafx.application.Platform;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
 import org.cyadd2nodes.AddTwoNodesTaskFactory;
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.service.util.AbstractCyActivator;
@@ -10,20 +16,44 @@ import org.cytoscape.work.TaskFactory;
 import java.util.Properties;
 
 
+@SuppressWarnings("restriction")
 public class CyActivator extends AbstractCyActivator {
 	public CyActivator() {
 		super();
 	}
 
 	public void start(BundleContext bc) {
-		CyApplicationManager cyApplicationManager = getService(bc, CyApplicationManager.class);
-		VisualMappingManager visualMappingManager = getService(bc, VisualMappingManager.class);
 		
-		AddTwoNodesTaskFactory addTwoNodesTaskFactory = new AddTwoNodesTaskFactory(cyApplicationManager, visualMappingManager);
-		Properties addTwoNodesTaskFactoryProps = new Properties();
-		addTwoNodesTaskFactoryProps.setProperty("preferredMenu","Apps.Samples");
-		addTwoNodesTaskFactoryProps.setProperty("title","Add two nodes");
-		registerService(bc,addTwoNodesTaskFactory,TaskFactory.class, addTwoNodesTaskFactoryProps);
+		try {
+			
+			// we have to be on JavaFX application thread
+			Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                	WebView webView = new WebView();
+        			WebEngine webEngine = webView.getEngine();
+        			
+                    JSObject win = (JSObject) webEngine.executeScript("window");        
+                }
+            });
+			
+			
+            
+		
+            /*
+			CyApplicationManager cyApplicationManager = getService(bc, CyApplicationManager.class);
+			VisualMappingManager visualMappingManager = getService(bc, VisualMappingManager.class);
+			
+			AddTwoNodesTaskFactory addTwoNodesTaskFactory = new AddTwoNodesTaskFactory(cyApplicationManager, visualMappingManager);
+			Properties addTwoNodesTaskFactoryProps = new Properties();
+			addTwoNodesTaskFactoryProps.setProperty("preferredMenu","Apps.Samples");
+			addTwoNodesTaskFactoryProps.setProperty("title","Add two nodes");
+			registerService(bc,addTwoNodesTaskFactory,TaskFactory.class, addTwoNodesTaskFactoryProps);
+			*/
 
+		} catch (Throwable e){
+			System.out.println("Could not start server!");
+			e.printStackTrace();
+		}
 	}
 }
