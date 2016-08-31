dacb8c110052d5bc86c9b02d58624e8706dce676
Matthias Koenig <konigmatt@googlemail.com>
2016-08-31 10:53:24 +0200

    implemented extension bundle

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # cy3javascript
 
 Testing `netscape.javascript` support in Cytoscape 3.
+
+Solution is the use of an extension bundle via
+https://github.com/diffplug/osgiX
+
+http://stackoverflow.com/questions/787985/apache-felix-what-are-extension-bundles
--- src/main/java/org/cy3javascript/CyActivator.java
+++ src/main/java/org/cy3javascript/CyActivator.java
@@ -1,23 +1,37 @@
 package org.cy3javascript;
 
 import org.cytoscape.service.util.AbstractCyActivator;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class CyActivator extends AbstractCyActivator {
-	public CyActivator() {
-		super();
-	}
+    public CyActivator() {
+        super();
+    }
 
-	public void start(BundleContext bc) {
-		
-		try {
-			System.out.println("Running JSObject access now");
-			
-			WebViewSwing.launch();
+    public void start(BundleContext bc) {
 
-		} catch (Throwable e){
-			System.out.println("Could not start server!");
-			e.printStackTrace();
-		}
-	}
+        try {
+            // Loading the extension bundle from resources
+            System.out.println("----------------------------");
+            System.out.println("Install extension bundle");
+            Bundle bundle = bc.getBundle();
+            URL jarUrl = bundle.getEntry("extension/org.cy3javascript.extension-0.0.1.jar");
+            InputStream input = jarUrl.openStream();
+            Bundle interfaces = bc.installBundle(jarUrl.getPath(), input);
+            input.close();
+            System.out.println("----------------------------");
+
+            // now we should be able to access netscape.javascript
+            System.out.println("Accessing netscape.javascript");
+            WebViewSwing.launch();
+
+        } catch (Throwable e) {
+            System.out.println("Could not start server!");
+            e.printStackTrace();
+        }
+    }
 }
